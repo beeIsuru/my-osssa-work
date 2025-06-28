3ada9110e0e7e73a39119cd1a893afae1650d1d2
beeIsuru <beeisuru@gmail.com>
2025-06-28 23:00:23 +0530

    Create ex.java

---
+++ labsheet 1/ex.java
@@ -0,0 +1,9 @@
+public class ex {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world1");
+        System.out.println("It's been nice knowing you.");// hello how
+        System.out.println("Goodbye world!");
+
+    }
+}
